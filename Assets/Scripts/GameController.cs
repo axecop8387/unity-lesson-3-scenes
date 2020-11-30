36ca3a2f2886f327e6abf67f54a2759a3191b26b
r.akhmetgaleev@galament.com <M0unt3d!>
2020-11-30 22:31:00 +0400

    init

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public CanvasGroup buttonPanel;
+    public Button button;
+    public Character[] playerCharacter;
+    public Character[] enemyCharacter;
+    Character currentTarget;
+    bool waitingForInput;
+
+    public Button restartLevelButton;
+    public Button openMainMenuButton;
+    public Button topLeftMenuButton;
+    public CanvasGroup topLeftMenuContainer;
+
+    public CanvasGroup resultBanner;
+    public TMP_Text resultBannerText;
+    public Button resultBannerRestartButton;
+    public Button resultBannerMainMenuButton;
+
+
+    Character FirstAliveCharacter(Character[] characters)
+    {
+        // LINQ: return enemyCharacter.FirstOrDefault(x => !x.IsDead());
+        foreach (var character in characters)
+        {
+            if (!character.IsDead())
+                return character;
+        }
+        return null;
+    }
+   
+    void PlayerWon()
+    {
+        /*StopAllCoroutines();*/
+
+        GameObject.Find("Canvas").GetComponent<CanvasGroup>().alpha = 1f;
+        Utility.SetCanvasGroupEnabled(buttonPanel, false);
+        Utility.SetCanvasGroupEnabled(topLeftMenuContainer, false);
+        
+
+        resultBanner.alpha = 1f;
+        resultBannerText.text = "Victory";
+        
+    }
+
+    void PlayerLost()
+    {
+        /*StopAllCoroutines();*/
+
+        GameObject.Find("Canvas").GetComponent<CanvasGroup>().alpha = 1f;
+        Utility.SetCanvasGroupEnabled(buttonPanel, false);
+        Utility.SetCanvasGroupEnabled(topLeftMenuContainer, false);
+        
+
+        resultBanner.alpha = 1f;
+        resultBannerText.text = "Defeat";
+        
+    }
+
+    bool CheckEndGame()
+    {
+        if (FirstAliveCharacter(playerCharacter) == null)
+        {
+            PlayerLost();
+            return true;
+        }
+
+        if (FirstAliveCharacter(enemyCharacter) == null)
+        {
+            PlayerWon();
+            return true;
+        }
+
+        return false;
+    }
+
+    void PlayerAttack()
+    {
+        waitingForInput = false;
+    }
+
+    public void NextTarget()
+    {
+        int index = Array.IndexOf(enemyCharacter, currentTarget);
+        for (int i = 1; i < enemyCharacter.Length; i++)
+        {
+            int next = (index + i) % enemyCharacter.Length;
+            if (!enemyCharacter[next].IsDead())
+            {
+                currentTarget.targetIndicator.gameObject.SetActive(false);
+                currentTarget = enemyCharacter[next];
+                currentTarget.targetIndicator.gameObject.SetActive(true);
+                return;
+            }
+        }
+    }
+
+    IEnumerator GameLoop()
+    {
+        yield return null;
+        while (!CheckEndGame())
+        {
+            foreach (var player in playerCharacter)
+            {
+                if (!player.IsDead())
+                {
+                    currentTarget = FirstAliveCharacter(enemyCharacter);
+                    if (currentTarget == null)
+                        break;
+
+                    currentTarget.targetIndicator.gameObject.SetActive(true);
+                    Utility.SetCanvasGroupEnabled(buttonPanel, true);
+
+                    waitingForInput = true;
+                    while (waitingForInput)
+                        yield return null;
+
+                    Utility.SetCanvasGroupEnabled(buttonPanel, false);
+                    currentTarget.targetIndicator.gameObject.SetActive(false);
+
+                    player.target = currentTarget.transform;
+                    player.AttackEnemy();
+
+                    while (!player.IsIdle())
+                        yield return null;
+
+                    break;
+                }
+            }
+
+            foreach (var enemy in enemyCharacter)
+            {
+                if (!enemy.IsDead())
+                {
+                    Character target = FirstAliveCharacter(playerCharacter);
+                    if (target == null)
+                        break;
+
+                    enemy.target = target.transform;
+                    enemy.AttackEnemy();
+
+                    while (!enemy.IsIdle())
+                        yield return null;
+
+                    break;
+                }
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(PlayerAttack);
+
+        initSceneCharacters();
+
+        Utility.SetCanvasGroupEnabled(buttonPanel, true);
+        resultBanner.alpha = 0.0f;
+
+        StartCoroutine(GameLoop());
+    }
+
+    private void initSceneCharacters()
+    {
+        var charactersContainer = GameObject.Find("Characters").transform;
+        if (charactersContainer != null)
+        {
+            var charactersCountInScene = charactersContainer.childCount;
+            if (charactersCountInScene > 0)
+            {
+                for (int index = 0; index < charactersCountInScene; index++)
+                {
+                    var character = charactersContainer.GetChild(index);
+
+                    if (character.name.Contains("Hooligan"))
+                    {
+                        playerCharacterList.Add(character.GetComponent<Character>());
+                    }
+
+                    if (character.name.Contains("Policeman"))
+                    {
+                        computerCharacterList.Add(character.GetComponent<Character>());
+                    }
+                }
+            }
+        }
+    }
+
+    public List<Character> computerCharacterList = new List<Character>();
+    public List<Character> playerCharacterList = new List<Character>();
+
+    public void OnTopLeftButtonClicked()
+    {
+        var isTopLefContainerVisible = topLeftMenuContainer.alpha > 0;
+        if (isTopLefContainerVisible)
+        {
+            topLeftMenuButton.GetComponentInChildren<TMP_Text>().text = "Pause";
+            Utility.SetCanvasGroupEnabled(topLeftMenuContainer, false);
+        }
+        else
+        {
+            topLeftMenuButton.GetComponentInChildren<TMP_Text>().text = "Resume";
+            Utility.SetCanvasGroupEnabled(topLeftMenuContainer, true);
+        }
+    }
+
+    public void RestartLevel()
+    {
+        LoadingScreen.instance.LoadScene("SampleScene");
+    }
+
+    public void OpenMainMenu()
+    {
+        LoadingScreen.instance.LoadScene("MainMenu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    enum Screen
+    {
+        None,
+        Main,
+        Settings,
+    }
+
+    public CanvasGroup mainScreen;
+    public CanvasGroup settingsScreen;
+
+    void SetCurrentScreen(Screen screen)
+    {
+        Utility.SetCanvasGroupEnabled(mainScreen, screen == Screen.Main);
+        Utility.SetCanvasGroupEnabled(settingsScreen, screen == Screen.Settings);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCurrentScreen(Screen.Main);
+    }
+
+    public void StartNewGame()
+    {
+        SetCurrentScreen(Screen.None);
+        LoadingScreen.instance.LoadScene("SampleScene");
+    }
+
+    public void StartLevelOne()
+    {
+        StartLevel(1);
+    }
+
+    public void StartLevelTwo()
+    {
+        StartLevel(2);
+    }
+
+    private void StartLevel(int levelNumber)
+    {
+        switch (levelNumber) {
+            case 1:
+                SetCurrentScreen(Screen.None);
+                LoadingScreen.instance.LoadScene("SampleScene");
+                break;
+            case 2:
+                SetCurrentScreen(Screen.None);
+                LoadingScreen.instance.LoadScene("SampleScene1");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void OpenSettings()
+    {
+        SetCurrentScreen(Screen.Settings);
+    }
+
+    public void CloseSettings()
+    {
+        SetCurrentScreen(Screen.Main);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
